8fefc785b4f4a98b8ab078ac21081ab002a41153
Greg <ggundersen@gmail.com>
2014-12-08 17:31:08 -0500

    Reorganized file structure and stylized site

--- webpage/index.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-
-var app = express();
-app.use('/static', express.static(__dirname + '/static'));
-app.use('/', express.static(__dirname + '/public'));
-
-app.listen(8000);
