3712864bab03db19234b74ceeb60f92cf1de512d
Greg <ggundersen@gmail.com>
2015-01-14 18:25:09 -0500

    Carousel works with Angular and stylized

--- js/carousel.js
+++ js/carousel.js
@@ -3,19 +3,19 @@ var mod = angular.module("carouselMod", []);
 mod.controller("carouselCtrl", ["$scope", "$timeout", function($scope, $timeout) {
     $scope.slides = [
         {
-            'index': 0,
+            'index': '0',
             'image': '/images/carousel/summer-research.png',
-            'title': '',
-            'description': '',
-            'link': '/#/summerresearch',
+            'title': 'Summer Research Training Program in Biomedical Big Data Science',
+            'caption': 'Apply for an research intensive ten-week training program offered at six sites affiliated with our NIH-funded BD2K Center.',
+            'link': '/#/education/summerresearch',
             'target': '_self',
             'id': 'carousel-btn-0'
         },
         {
-            index: 1,
+            'index': '1',
             'image': '/images/carousel/big-data.png',
             'title': '',
-            'description': '',
+            'caption': 'whajsjdfsfjd',
             'link': '/#/summerresearch',
             'target': '_self',
             'id': 'carousel-btn-0'
