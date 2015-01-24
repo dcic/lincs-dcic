db7504276d4f0f7d77f206231111aefff2be0834
Greg <ggundersen@gmail.com>
2015-01-23 21:49:00 -0500

    Cleaned up front page and summer research page

--- js/data/team.js
+++ js/data/team.js
@@ -36,126 +36,140 @@ mod.controller("teamCtrl", ["$scope", function($scope) {
         }
     ];
 
-	$scope.icahn = [
+	$scope.groups = [
         {
-            firstName: "Gregory",
-            lastName: "Gundersen",
-            affiliation: ISMMS,
-            email: "gregory.gundersen {at} mssm.edu"
+	        name: "Icahn School of Medicine at Mount Sinai",
+	        members: [
+                {
+                    firstName: "Gregory",
+                    lastName: "Gundersen",
+                    affiliation: ISMMS,
+                    email: "gregory.gundersen {at} mssm.edu"
+                },
+                {
+                    firstName: "Qiaonan",
+                    lastName: "Duan",
+                    affiliation: ISMMS,
+                    email: "qiaonan.duan {at} mssm.edu"
+                },
+                {
+                    firstName: "Matthew",
+                    lastName: "Jones",
+                    affiliation: ISMMS,
+                    email: "matthew.jones {at} mssm.edu"
+                },
+                {
+                    firstName: "Yan",
+                    lastName: "Kou",
+                    affiliation: ISMMS,
+                    email: "yan.kou {at} mssm.edu"
+                },
+                {
+                    firstName: "Nicolas",
+                    lastName: "Fernandez",
+                    affiliation: ISMMS,
+                    email: "nicolas.fernandez {at} mssm.edu"
+                },
+                {
+                    firstName: "Zichen",
+                    lastName: "Wang",
+                    affiliation: ISMMS,
+                    email: "zichen.wang {at} mssm.edu"
+                },
+                {
+                    firstName: "Andrew",
+                    lastName: "Rouillard",
+                    affiliation: ISMMS,
+                    email: "andrew.rouillard {at} mssm.edu"
+                },
+                {
+                    firstName: "Neil",
+                    lastName: "Clark",
+                    affiliation: ISMMS,
+                    email: "neil.clark {at} mssm.edu"
+                },
+                {
+                    firstName: "Sherry",
+                    lastName: "Jenkins",
+                    affiliation: ISMMS,
+                    email: "sherry.jenkins {at} mssm.edu"
+                }
+            ]
         },
         {
-            firstName: "Qiaonan",
-            lastName: "Duan",
-            affiliation: ISMMS,
-            email: "qiaonan.duan {at} mssm.edu"
+            name: "University of Cincinnati",
+            members: [
+                {
+                    firstName: "Jarek",
+                    lastName: "Meller ",
+                    affiliation: "University of Cincinnati",
+                    email: "jmeller {at} cchmc.org"
+                },
+                {
+                    name: "Michal",
+                    lastName: "Kouril",
+                    affiliation: "University of Cincinnati",
+                    email: "michal.kouril {at} cchmc.org"
+                }
+            ]
         },
         {
-            firstName: "Matthew",
-            lastName: "Jones",
-            affiliation: ISMMS,
-            email: "matthew.jones {at} mssm.edu"
+            name: "University of Miami",
+            members: [
+                {
+                    firstName: "Christopher",
+                    lastName: "Mader",
+                    affiliation: UM,
+                    email: "cmader {at} miami.edu"
+                },
+                {
+                    firstName: "Amar",
+                    lastName: "Koleti",
+                    affiliation: UM,
+                    email: "akoleti {at} miami.edu"
+                },
+                {
+                    firstName: "Caty",
+                    lastName: "Chung",
+                    affiliation: UM,
+                    email: "c.chung {at} med.miami.edu"
+                },
+                {
+                    firstName: "Deepthi",
+                    lastName: "Puram",
+                    affiliation: UM,
+                    email: "d.puram {at} miami.edu"
+                },
+                {
+                    firstName: "Dusica",
+                    lastName: "Vidovic",
+                    affiliation: UM,
+                    email: "dvidovic {at} med.miami.edu"
+                }
+            ]
         },
         {
-            firstName: "Yan",
-            lastName: "Kou",
-            affiliation: ISMMS,
-            email: "yan.kou {at} mssm.edu"
-        },
-        {
-            firstName: "Nicolas",
-            lastName: "Fernandez",
-            affiliation: ISMMS,
-            email: "nicolas.fernandez {at} mssm.edu"
-        },
-        {
-            firstName: "Zichen",
-            lastName: "Wang",
-            affiliation: ISMMS,
-            email: "zichen.wang {at} mssm.edu"
-        },
-        {
-            firstName: "Andrew",
-            lastName: "Rouillard",
-            affiliation: ISMMS,
-            email: "andrew.rouillard {at} mssm.edu"
-        },
-        {
-            firstName: "Neil",
-            lastName: "Clark",
-            affiliation: ISMMS,
-            email: "neil.clark {at} mssm.edu"
-        },
-        {
-            firstName: "Sherry",
-            lastName: "Jenkins",
-            affiliation: ISMMS,
-            email: "sherry.jenkins {at} mssm.edu"
-        }
-    ];
-	
-    $scope.investigators = [
-        {
-            name: "Kayee Yeung",
-            affiliation: "University of Washington",
-            email: "kayee {at} u.washington.edu"
-        },
-        {
-            name: "Ziv Bar-Joseph",
-            affiliation: "Carnegie-Mellon University",
-            email: "zivbj {at} cs.cmu.edu"
-        },
-        {
-            name: "Peter Hornbeck",
-            affiliation: "Cell Signaling Inc.",
-            email: "phornbeck {at} cellsignal.com"
-        }
-    ];
-	
-    $scope.cinncinati = [];
-	
-	$scope.miami = [
-        {
-            firstName: "Christopher",
-            lastName: "Mader",
-            affiliation: UM,
-            email: "cmader {at} miami.edu"
-        },
-        {
-            firstName: "Amar",
-            lastName: "Koleti",
-            affiliation: UM,
-            email: "akoleti {at} miami.edu"
-        },
-        {
-            firstName: "Caty",
-            lastName: "Chung",
-            affiliation: UM,
-            email: "c.chung {at} med.miami.edu"
-        },
-        {
-            firstName: "Deepthi",
-            lastName: "Puram",
-            affiliation: UM,
-            email: "d.puram {at} miami.edu"
-        },
-        {
-            firstName: "Dusica",
-            lastName: "Vidovic",
-            affiliation: UM,
-            email: "dvidovic {at} med.miami.edu"
+            name: "Investigators of External Data Science Research Projects",
+            members: [
+                {
+                    firstName: "Kayee",
+                    lastName: "Yeung",
+                    affiliation: "University of Washington",
+                    email: "kayee {at} u.washington.edu"
+                },
+                {
+                    firstName: "Ziv",
+                    lastName: "Bar-Joseph",
+                    affiliation: "Carnegie-Mellon University",
+                    email: "zivbj {at} cs.cmu.edu"
+                },
+                {
+                    firstName: "Peter",
+                    lastName: "Hornbeck",
+                    affiliation: "Cell Signaling Inc.",
+                    email: "phornbeck {at} cellsignal.com"
+                }
+            ]
         }
     ];
 }]);
-
-/*
-{
-    name: "Jarek Meller ",
-    affiliation: "Cincinnati Children's Hospital Medical Center",
-    email: "jmeller {at} cchmc.org"
-},
-{
-    name: "John Reichard",
-    affiliation: "Toxicology Excellence for Risk Assessment (TERA)",
-    email: "jmeller {at} cchmc.org"
-}
-*/
