f26bea02931abadcbac3b73d07c6efadcb1db9d9
Greg <ggundersen@gmail.com>
2015-09-08 13:52:59 -0400

    Added Research Projects to carousel

--- js/data/carousel.js
+++ js/data/carousel.js
@@ -1,19 +1,29 @@
 var mod = angular.module("carouselMod", []);
 
 mod.controller("carouselCtrl", ["$scope", function($scope) {
+
+    var DIR = '/images/carousel/';
+
     $scope.slides = [
         {
             index: 0,
-            image: '/images/carousel/bubble-graph.png',
-            title: 'Big Data Science',
+            image: DIR + 'bubble-graph.png',
+            title: 'Big Data Science Course',
             caption: 'Learn various methods of analysis of LINCS data and other relevant Big Data with the DCIC\'s MOOC on Coursera.',
             link: 'https://www.coursera.org/course/bd2klincs',
             target: '_blank'
         },
         {
             index: 1,
-            image: '/images/carousel/summer-fellows-2015.png',
-            title: 'Summer Research',
+            image: DIR + 'dag.png',
+            title: 'Research Projects',
+            caption: 'The BD2K-LINCS DCIC is conducting research by integrating and analyzing various big data resources.',
+            link: '#/summer-fellows-2015'
+        },
+        {
+            index: 2,
+            image: DIR + 'summer-fellows-2015.png',
+            title: 'Summer Research Program',
             caption: 'BD2K-LINCS DCIC summer fellows complete a ten-week intensive research experience within our Center.',
             link: '#/summer-fellows-2015'
         }
