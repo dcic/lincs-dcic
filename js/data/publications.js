40741a74d0bcfbe889313182d16ffe52b977e939
Greg <ggundersen@gmail.com>
2015-09-29 17:51:48 -0400

    Updated G2E publication

--- js/data/publications.js
+++ js/data/publications.js
@@ -38,7 +38,7 @@ mod.controller("publicationsCtrl", ["$scope", function($scope) {
             title: "GEO2Enrichr: browser extension and server app to extract gene sets from GEO and analyze them for biological functions",
             url: "http://www.ncbi.nlm.nih.gov/pubmed/25971742",
             authors: "Gundersen GW, Jones MR, Rouillard AD, Kou Y, Monteiro CD, Feldmann AS, Hu KS, Ma'ayan A.",
-            citation: "Bioinformatics 2015 May 13. pii: btv297.",
+            citation: "Bioinformatics. 2015 Sep 15;31(18):3060-2.",
             ids: "PMID: 25971742",
             year: 2015
 	    },
