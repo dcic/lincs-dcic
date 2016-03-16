3d29ecbf17d837a365b71d9324728329e896a6e8
skoplev <skoplev@gmail.com>
2016-03-15 22:57:36 -0400

    Fixed space/tabs indentation.

--- js/data/news.js
+++ js/data/news.js
@@ -2,61 +2,61 @@ var mod = angular.module("newsMod", []);
 
 mod.controller("newsCtrl", ["$scope", "$sce", function($scope, $sce) {
 
-    var DIR = '/images/news/';
+	var DIR = '/images/news/';
 
 	var formatStories = function(stories) {
-        return _.sortBy(stories, function(story) {
-            return -Date.parse(story.date);
-        });
-    };
+		return _.sortBy(stories, function(story) {
+			return -Date.parse(story.date);
+		});
+	};
 
 	$scope.stories = formatStories([
-        {
-            title: "BD2K-LINCS DCIC's First Annual Systems Biology Data Science Symposium",
-            source: "University of Miami News",
-            url: "http://med.miami.edu/news/sylvester-director-delivers-keynote-address-at-first-annual-systems-biology",
-            target: "_blank",
-            date: "2/23/2016",
-            mainText: "",
-            image: DIR + "dcic_symposium_group.png"
-        },
-	 	{
-            title: "Genetics: Big Hopes for Big Data",
-            source: "Nature | Outlook",
-            url: "http://www.nature.com/nature/journal/v527/n7578_supp/full/527S108a.html",
-            target: "_blank",
-            date: "11/19/2015",
-            mainText: "",
-            image: DIR + "nature_outlook_cover.png"
-        },
-	 	{
-            title: "\"The characteristic direction: a geometrical approach to identify differentially expressed genes\" has been recommended in F1000Prime as being of special significance in its field by F1000 Faculty Member Charles Auffray",
-            source: "F1000 Prime",
-            url: "http://f1000.com/prime/718324928?subscriptioncode=73f1be84-cf1b-47e0-87d4-8ce34f95c9fc",
-            target: "_blank",
-            date: "3/10/2015",
-            mainText: "",
-            image: DIR + "chdir.png"
-        },
-        {
-            title: "NIH Launches a United Ecosystem for Big Data",
-		    source: "Biomedical Computation Review",
+		{
+			title: "BD2K-LINCS DCIC's First Annual Systems Biology Data Science Symposium",
+			source: "University of Miami News",
+			url: "http://med.miami.edu/news/sylvester-director-delivers-keynote-address-at-first-annual-systems-biology",
+			target: "_blank",
+			date: "2/23/2016",
+			mainText: "",
+			image: DIR + "dcic_symposium_group.png"
+		},
+		{
+			title: "Genetics: Big Hopes for Big Data",
+			source: "Nature | Outlook",
+			url: "http://www.nature.com/nature/journal/v527/n7578_supp/full/527S108a.html",
+			target: "_blank",
+			date: "11/19/2015",
+			mainText: "",
+			image: DIR + "nature_outlook_cover.png"
+		},
+		{
+			title: "\"The characteristic direction: a geometrical approach to identify differentially expressed genes\" has been recommended in F1000Prime as being of special significance in its field by F1000 Faculty Member Charles Auffray",
+			source: "F1000 Prime",
+			url: "http://f1000.com/prime/718324928?subscriptioncode=73f1be84-cf1b-47e0-87d4-8ce34f95c9fc",
+			target: "_blank",
+			date: "3/10/2015",
+			mainText: "",
+			image: DIR + "chdir.png"
+		},
+		{
+			title: "NIH Launches a United Ecosystem for Big Data",
+			source: "Biomedical Computation Review",
 			url: "http://biomedicalcomputationreview.org/content/nih-launches-united-ecosystem-big-data",
 			target: "_blank",
 			date: "1/8/2015",
-		    mainText: "Francis Collins, MD, PhD, Director of the National Institutes of Health (NIH), says he used to feel \"data envy\" toward the field of physics. In those days, \"no one would have predicted that biology would emerge as the biggest challenge in terms of data. But that is now the case.\"",
-		    image: DIR + "biomedical-computation-review.png"
-        },
+			mainText: "Francis Collins, MD, PhD, Director of the National Institutes of Health (NIH), says he used to feel \"data envy\" toward the field of physics. In those days, \"no one would have predicted that biology would emerge as the biggest challenge in terms of data. But that is now the case.\"",
+			image: DIR + "biomedical-computation-review.png"
+		},
 		{
 			title: "UC Part of Team Selected for NIH's Multimillion-Dollar 'Big Data' Push",
-		    source: "University of Cincinnati Health News",
+			source: "University of Cincinnati Health News",
 			url: "http://healthnews.uc.edu/news/?/25274/",
 			target: "_blank",
 			date: "10/9/2014"
 		},
 		{
 			title: "Mount Sinai researchers awarded one of the Big Data to Knowledge (BD2K) Centers of Excellence grants",
-		    source: "Icahn School of Medicine at Mount Sinai",
+			source: "Icahn School of Medicine at Mount Sinai",
 			url: "http:/icahn.mssm.edu/research/labs/maayan-laboratory/about-us/news/center-to-seek-new-therapeutics-by-integrating-gene-protein-databases-",
 			target: "_blank",
 			date: "10/9/2014"
